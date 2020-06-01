46844ffbd7d08f2740e28b8e62df8e83546bdda1
ZoondEngine <mango.kamchatka.1997@gmail.com>
2020-06-01 03:15:47 +0300

    [master-rt-0145958]: added plugins support with hash security

---
+++ Doze/Plugins/DozePlugin.cs
@@ -0,0 +1,62 @@
+﻿using Doze.Plugins.Contracts;
+using System;
+using System.Collections.Generic;
+
+namespace Doze.Plugins
+{
+    public abstract class DozePlugin : IDozePlugin
+    {
+        private PluginsObject PluginManager { get; }
+
+        public string PluginName { get; set; }
+        public Version PluginVersion { get; private set; }
+        public string PluginDescription { get; private set; }
+
+        public DozePlugin(string name, Version version, string description)
+        {
+            PluginName = name;
+            PluginVersion = version;
+            PluginDescription = description;
+
+            PluginManager = DozeObject.FindObjectOfType<PluginsObject>();
+        }
+
+        public abstract void OnInitialized();
+        public abstract void OnStopped();
+        public abstract void OnPluginReady();
+        public abstract void OnPluginBeforeDestroy();
+
+        public abstract int GetPluginHashCode();
+
+        public void Call(string hook, params object[] args)
+        {
+            CallReturnable(hook, args);
+        }
+
+        public object CallReturnable(string hook, params object[] args)
+        {
+            List<Type> argsTypes = new List<Type>();
+            foreach (var arg in args)
+            {
+                argsTypes.Add(arg.GetType());
+            }
+
+            var method = GetType().GetMethod(hook, argsTypes.ToArray());
+            if (method != null)
+            {
+                return method.Invoke(this, args);
+            }
+
+            return default;
+        }
+
+        public string GetName()
+            => PluginName;
+
+        public Version GetVersion()
+            => PluginVersion;
+
+        public string GetDescription()
+            => PluginDescription;
+    }
+}
---
+++ Doze/Plugins/PluginLoader.cs
@@ -0,0 +1,68 @@
+﻿using Doze.Plugins.Contracts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Doze.Plugins
+{
+    internal class PluginLoader
+    {
+        private static readonly int AccessPluginHashCode = 0x00480038;
+
+        public List<IDozePlugin> Plugins { get; set; }
+        public string PluginsDir { get; set; }
+        public string PluginsExtension { get; set; }
+
+        public PluginLoader(string dir, string extension)
+        {
+            PluginsDir = dir;
+            PluginsExtension = extension;
+            Plugins = new List<IDozePlugin>();
+        }
+
+        public bool Load(string plugin)
+        {
+            var assembly = Assembly.LoadFrom($"{PluginsDir}\\{plugin}.{PluginsExtension}");
+            if(assembly != null)
+            {
+                var code = (int)assembly.GetType("LoaderAccessor").GetProperty("PluginHashCode").GetValue(null, null);
+                if(code == AccessPluginHashCode)
+                {
+                    var plugins = assembly.GetTypes().Where((x) => x.GetInterfaces().Contains(typeof(IDozePlugin)))
+                                .Select((x) => x.GetConstructor(Type.EmptyTypes)?.Invoke(null) as IDozePlugin)
+                                .ToList();
+
+                    foreach (var item in plugins)
+                    {
+                        Plugins.Add(item);
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public void LoadAll()
+        {
+            var files = Directory.GetFiles(PluginsDir, PluginsExtension);
+            if(files.Length > 0)
+            {
+                foreach(var file in files)
+                {
+                    Load(ParsePluginName(file));
+                }
+            }
+        }
+
+        public DozePlugin GetPlugin(string name)
+            => (DozePlugin)Plugins.FirstOrDefault((x) => x.GetName().ToLower() == name.ToLower());
+        public T GetPlugin<T>(string name) where T : DozePlugin
+            => (T)GetPlugin(name) ?? default;
+
+        private string ParsePluginName(string path)
+            => path.Split('\\').Last().Split('.')[0];
+    }
+}
